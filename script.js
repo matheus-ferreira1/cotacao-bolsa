27fb8ebb568029ab64c43c78bb3db303f653186f
Matheus Ferreira <matheus.fer33@gmail.com>
2022-03-14 18:06:58 -0300

    estilização adicionada e lógica refinada (com mudança de cores quando a cotação é exibida)

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function fillForm(dados) {
+    if (dados.change_percent < 0) {
+        document.getElementById('variacao').className = 'goRed';
+        document.getElementById('cotacao').className = 'goRed'
+    } else {
+        document.getElementById('variacao').className = 'goGreen'
+        document.getElementById('cotacao').className = 'goGreen'
+    }
     document.getElementById('nome').value = dados.name;
     document.getElementById('cotacao').value = ` R$ ${dados.price}`;
     document.getElementById('variacao').value = `${dados.change_percent}%`;
@@ -10,19 +17,22 @@ async function fazerCotacao() {
 
     const url = `https://api.hgbrasil.com/finance/stock_price?format=json-cors&key=9211c9ea&symbol=${papel}`
 
-    let request = new XMLHttpRequest()
-    request.open("GET", url, false)
-    request.send()
-    let bruto =  JSON.parse(request.responseText)
-    let dadosRaw = bruto.results
-    let dados = Object.values(dadosRaw)
-    dados = dados[0]
-    console.log(dados)
-    
-    if (dados.hasOwnProperty('error')) {
-        alert('Ativo não encontrado')
+    if (papel == '') {
+        alert('Por favor, insira um ativo')
     } else {
-        fillForm(dados)
+        let request = new XMLHttpRequest()
+        request.open("GET", url, false)
+        request.send()
+        let bruto =  JSON.parse(request.responseText)
+        let dadosRaw = bruto.results
+        let dados = Object.values(dadosRaw)
+        dados = dados[0]
+        
+        if (dados.hasOwnProperty('error')) {
+            alert('Ativo não encontrado')
+        } else {
+            fillForm(dados)
+        }
     }
 }
 
